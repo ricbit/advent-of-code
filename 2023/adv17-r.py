043be8f18deb2882bfb60f6229e5b07f95173f65
Ricardo Bittencourt <bluepenguin@gmail.com>
2023-12-17 04:20:41 -0300

    Implemented A*

--- 2023/adv17-r.py
+++ 2023/adv17-r.py
@@ -2,15 +2,18 @@ import sys
 import aoc
 import heapq
 
+def h(m, y, x):
+  return (m.h - y + m.w - x)
+
 def search(m, minsize, maxsize):
   pos = (0, 0)
   lastdir = (2, 2)
   lastsize = 5
   vec = (pos, lastdir, lastsize)
   visited = set([vec])
-  vnext = [(0,) + vec]
+  vnext = [(h(m, 0, 0),0,) + vec]
   while vnext:
-    score, (y, x), (dy, dx), lastsize = heapq.heappop(vnext)
+    heu, score, (y, x), (dy, dx), lastsize = heapq.heappop(vnext)
     if (y, x) == (m.h - 1, m.w - 1):
       return score
     for j, i in m.iter_neigh4(y, x):
@@ -25,7 +28,7 @@ def search(m, minsize, maxsize):
           continue
         if (vec := ((j, i), (dj, di), 1)) in visited:
           continue
-      heapq.heappush(vnext, (score + m[j][i],) + vec)
+      heapq.heappush(vnext, (h(m,j,i) +score + m[j][i] ,score+m[j][i]) + vec)
       visited.add(vec)
 
 lines = [[int(i) for i in line.strip()] for line in sys.stdin]

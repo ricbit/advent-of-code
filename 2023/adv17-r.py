b2941fea7f2bea7aad9cf3b144f6e78d63bd4d67
Ricardo Bittencourt <bluepenguin@gmail.com>
2023-12-17 22:12:46 -0300

    A*, short state

--- 2023/adv17-r.py
+++ 2023/adv17-r.py
@@ -3,36 +3,38 @@ import aoc
 import heapq
 
 def heuristic(m, y, x):
-  return (m.h - y + m.w - x)
+  return m.h - y + m.w - x
 
 def search(m, minsize, maxsize):
   pos = (0, 0)
   lastdir = (2, 2)
-  lastsize = 5
-  vec = (pos, lastdir, lastsize)
-  visited = set([vec])
+  vec = (pos, lastdir)
+  visited = set()
   vnext = [(heuristic(m, 0, 0), 0) + vec]
   while vnext:
-    heu, score, (y, x), (dy, dx), lastsize = heapq.heappop(vnext)
+    old_heuristic, score, (y, x), (dy, dx) = heapq.heappop(vnext)
+    if (oldvec := ((y, x), (dy, dx))) in visited:
+      continue
+    visited.add(oldvec)
     if (y, x) == (m.h - 1, m.w - 1):
       return score
     for j, i in m.iter_neigh4(y, x):
-      dj, di = y - j, x - i
-      if (dj, di) == (dy, dx):
-        if lastsize >= maxsize:
+      dj, di = j - y, i - x
+      if (dj, di) == (dy, dx) or (dj, di) == (-dy, -dx):
           continue
-        if (vec := ((j, i), (dj, di), lastsize + 1)) in visited:
+      dscore = 0
+      for size in range(1, maxsize + 1):
+        nj, ni = y + size * dj, x + size * di
+        if not m.valid(nj, ni):
+          break
+        dscore += m[nj][ni]
+        if size < minsize or (vec := ((nj, ni), (dj, di))) in visited:
           continue
-      else:
-        if lastsize < minsize or (dj, di) == (-dy, -dx):
-          continue
-        if (vec := ((j, i), (dj, di), 1)) in visited:
-          continue
-      pure_score = score + m[j][i]
-      heapq.heappush(vnext, (heuristic(m,j,i) + pure_score, pure_score) + vec)
-      visited.add(vec)
+        pure_score = score + dscore
+        new_heuristic = heuristic(m, nj, ni) + pure_score
+        heapq.heappush(vnext, (new_heuristic, pure_score) + vec)
 
 lines = [[int(i) for i in line.strip()] for line in sys.stdin]
 m = aoc.Table(lines)
-print(search(m, 0, 3))
+print(search(m, 1, 3))
 print(search(m, 4, 10))

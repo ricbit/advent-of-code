0d415dc31ee80108df7c777db220ea4ac7e1542b
Ricardo Bittencourt <bluepenguin@gmail.com>
2023-12-19 05:12:29 -0300

    Prune search tree

--- 2023/adv17-r.py
+++ 2023/adv17-r.py
@@ -1,6 +1,7 @@
 import sys
 import aoc
 import heapq
+import cProfile
 
 def heuristic(m, y, x):
   return m.h - y + m.w - x
@@ -10,18 +11,24 @@ def search(m, minsize, maxsize):
   lastdir = (2, 2)
   vec = (pos, lastdir)
   visited = set()
+  current = {}
   vnext = [(heuristic(m, 0, 0), 0) + vec]
+  count = 0
+  skipped = 0
   while vnext:
+    count += 1
     old_heuristic, score, (y, x), (dy, dx) = heapq.heappop(vnext)
     if (oldvec := ((y, x), (dy, dx))) in visited:
+      skipped += 1
       continue
     visited.add(oldvec)
     if (y, x) == (m.h - 1, m.w - 1):
+      print(count, skipped)
       return score
     for j, i in m.iter_neigh4(y, x):
       dj, di = j - y, i - x
       if (dj, di) == (dy, dx) or (dj, di) == (-dy, -dx):
-          continue
+        continue
       dscore = 0
       for size in range(1, maxsize + 1):
         nj, ni = y + size * dj, x + size * di
@@ -32,7 +39,9 @@ def search(m, minsize, maxsize):
           continue
         pure_score = score + dscore
         new_heuristic = heuristic(m, nj, ni) + pure_score
-        heapq.heappush(vnext, (new_heuristic, pure_score) + vec)
+        if new_heuristic < current.get(vec, 1e9):
+          heapq.heappush(vnext, (new_heuristic, pure_score) + vec)
+          current[vec] = new_heuristic
 
 lines = [[int(i) for i in line.strip()] for line in sys.stdin]
 m = aoc.Table(lines)

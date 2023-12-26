188df9df66482b11bffb302534f323d53f711fd4
Ricardo Bittencourt <bluepenguin@gmail.com>
2023-12-26 16:17:13 -0300

    Improve heurisitics on 25

--- 2023/adv25-r.py
+++ 2023/adv25-r.py
@@ -2,6 +2,7 @@ import sys
 import random
 from collections import *
 import copy
+import itertools
 
 def read_graph():
   verts = defaultdict(lambda: set())
@@ -66,19 +67,23 @@ def extract_edges(used):
   for (a, b), v in edges:
     if a not in verts and b not in verts:
       ans.append((a, b))
-      if len(ans) == 3:
+      if len(ans) == 6:
         return ans
 
 def search(verts):
   while True:
     used = Counter()
-    for i in range(100):
+    for i in range(300):
       random_bfs(verts, used)
     edges = extract_edges(used)
-    size = floodfill(verts, edges) 
-    ans = size * (len(verts) - size)
-    if ans > 0:
-      return ans
+    ans = [0]
+    for subset in itertools.combinations(edges, 3):
+      size = floodfill(verts, edges) 
+      compl = len(verts) - size
+      if size > len(verts) / 3 and compl > len(verts) / 3:
+        ans.append(size * compl)
+    if max(ans) > 0:
+      return max(ans)
 
 verts = read_graph()
 print(search(verts))

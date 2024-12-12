05fad8f855d9b39cf667816f569b6735c203fdc0
Ricardo Bittencourt <bluepenguin@gmail.com>
2024-12-12 13:50:24 -0300

    Refactor 2024 12

--- 2024/adv12-r.py
+++ 2024/adv12-r.py
@@ -1,6 +1,6 @@
 import aoc
 
-class Grow:
+class Regions:
   def __init__(self, t):
     self.t = t
     self.mask = t.copy()
@@ -8,14 +8,16 @@ class Grow:
   def common_sides(self, visited):
     ans = 0
     for j, i in visited:
-      if (j, i-1) in visited and (j-1, i) not in visited and (j-1, i-1) not in visited:
-        ans += 1
-      if (j, i-1) in visited and (j+1, i) not in visited and (j+1, i-1) not in visited:
-        ans += 1
-      if (j-1, i) in visited and (j, i-1) not in visited and (j-1, i-1) not in visited:
-        ans += 1
-      if (j-1, i) in visited and (j, i+1) not in visited and (j-1, i+1) not in visited:
-        ans += 1
+      # Left neighbour
+      if (j, i - 1) in visited:
+        for jj in [j - 1, j + 1]:
+          if (jj, i) not in visited and (jj, i - 1) not in visited:
+            ans += 1
+      # Up neighbour
+      if (j - 1, i) in visited:
+        for ii in [i - 1, i + 1]:
+          if (j, ii) not in visited and (j - 1, ii) not in visited:
+            ans += 1
     return ans
 
   def grow(self, y, x):
@@ -33,8 +35,7 @@ class Grow:
       for j, i in self.t.iter_neigh4(y, x):
         if (j, i) in visited:
           perimeter -= 2
-      for j, i in self.t.iter_neigh4(y, x):
-        if self.t[j][i] == self.t[y][x] and self.mask[j][i] != 0:
+        elif self.t[j][i] == self.t[y][x]:
           pnext.append((j, i))
     return area * perimeter, area * (perimeter - self.common_sides(visited))
 
@@ -45,9 +46,9 @@ class Grow:
         x, y =  self.grow(j, i)
         ans1 += x
         ans2 += y
-    return ans1,ans2
+    return ans1, ans2
 
 data = aoc.Table.read()
-part1, part2 = Grow(data.grow(0)).solve()
+part1, part2 = Regions(data.grow(0)).solve()
 aoc.cprint(part1)
 aoc.cprint(part2)

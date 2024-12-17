80835a6feb2edaec7f2548894ef3590e856d0839
Ricardo Bittencourt <bluepenguin@gmail.com>
2024-12-17 12:28:56 -0300

    Optimized 2024 17

--- 2024/adv17-r.py
+++ 2024/adv17-r.py
@@ -1,5 +1,6 @@
 import aoc
 import functools
+import multiprocessing
 
 def combo(regs, val):
   if val < 4:
@@ -15,14 +16,12 @@ def parse(data):
   program = aoc.ints(program[0].split(":")[1].strip().split(","))
   return regs, program
 
-def solve(regs, program):
-  pc = 0
-  ans = []
-  while True:
+def solve_part1(regs, program):
+  pc, ans = 0, []
+  while pc < len(program):
     match program[pc]:
       case 0: # adv
-        d = regs[0] // (2 ** combo(regs, program[pc + 1]))
-        regs[0] = d
+        regs[0] = regs[0] // (2 ** combo(regs, program[pc + 1]))
       case 1: # bxl
         regs[1] ^= program[pc + 1]
       case 2: # bst
@@ -30,31 +29,27 @@ def solve(regs, program):
       case 3: # jnz
         if regs[0] != 0:
           pc = program[pc + 1] - 2
-        pass
       case 4: # bxc
         regs[1] ^= regs[2]
       case 5: # out
         ans.append(combo(regs, program[pc + 1]) % 8)
       case 6: # bdv
-        d = regs[0] // (2 ** combo(regs, program[pc + 1]))
-        regs[1] = d
+        regs[1] = regs[0] // (2 ** combo(regs, program[pc + 1]))
       case 7: # cdv
-        d = regs[0] // (2 ** combo(regs, program[pc + 1]))
-        regs[2] = d
+        regs[2] = regs[0] // (2 ** combo(regs, program[pc + 1]))
     pc += 2
-    if pc >= len(program):
-      break
   return ",".join(str(i) for i in ans)
 
+@functools.cache
 def simulate(a):
   b, c = 0, 0
   ans = []
   while a:
     b = a % 8
-    b = b ^ 5
-    c = a // (2 ** b)
+    b = b ^ 5 
+    c = a >> b
     b = b ^ 6
-    a = a // 8
+    a = a >> 3
     b = b ^ c
     ans.append(b % 8)
   return ans
@@ -65,25 +60,27 @@ class Sim:
     self.minback = 1e20
 
   @functools.cache
-  def rec(self, back, shift):
-    if shift == 16:
+  def rec(self, x):
+    back, shift = x
+    if shift == len(self.prog):
       x = simulate(back)
-      if x[:shift] == self.prog[:shift]:
-        self.minback = min(self.minback, back)
+      if x == self.prog:
+        self.minback = min(back, self.minback)
       return
-    for i in range(256 * 8):
+    for i in range(128 * 8):
       stable = 2 ** (3 * shift)
       n = back + i * stable
       x = simulate(n)
       if x[:shift + 1] == self.prog[:shift + 1]:
-        self.rec(n % (2 ** (3 * shift + 3)), shift + 1)
+        if n < self.minback:
+          self.rec((n % (stable * 8), shift + 1))
 
   def search(self):
-    self.rec(0, 0)
+    self.rec((0, 0))
     return self.minback
 
 data = aoc.line_blocks()
 regs, program = parse(data)
-aoc.cprint(solve(regs, program))
+aoc.cprint(solve_part1(regs, program))
 s = Sim(program)
 aoc.cprint(s.search())

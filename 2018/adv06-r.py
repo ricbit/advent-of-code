0d944751ed02bb44ad0f004e10c76059acc8a31c
Ricardo Bittencourt <bluepenguin@gmail.com>
2025-01-08 22:23:41 -0300

    Problem 2018-06 under 1s

--- 2018/adv06-r.py
+++ 2018/adv06-r.py
@@ -1,6 +1,7 @@
 import sys
 import aoc
 from collections import Counter
+import multiprocessing
 
 def closest(j, i, points):
   kset, kval = set(), 1e6
@@ -12,24 +13,39 @@ def closest(j, i, points):
       kset.add((ky, kx))
   return kset
 
-def find_forbidden(points, bounds):
+def forbidden_mask_x(points, y, bounds):
+  mask = (closest(y, x, points) for x in [bounds.xmin, bounds.xmax])
+  return set.union(*mask)
+
+def forbidden_mask_y(points, x, bounds):
+  mask = (closest(y, x, points) for y in [bounds.ymin, bounds.ymax])
+  return set.union(*mask)
+
+def find_forbidden(points, bounds, pool):
   forbidden = set()
-  for y in range(bounds.ymin, bounds.ymax + 1):
-    for x in [bounds.xmin, bounds.xmax]:
-      forbidden |= closest(y, x, points)
-  for x in range(bounds.xmin, bounds.xmax + 1):
-    for y in [bounds.ymin, bounds.ymax]:
-      forbidden |= closest(y, x, points)
+  yrange = range(bounds.ymin, bounds.ymax + 1)
+  mask = pool.starmap(forbidden_mask_x, ((points, y, bounds) for y in yrange))
+  forbidden.update(*mask)
+  xrange = range(bounds.xmin, bounds.xmax + 1)
+  mask = pool.starmap(forbidden_mask_y, ((points, x, bounds) for x in xrange))
+  forbidden.update(*mask)
   return forbidden
 
-def solve1(points):
+def solve1_y(bounds, j, points, forbidden):
+  count = Counter()
+  for i in range(bounds.xmin, bounds.xmax + 1):
+    kset = closest(j, i, points)
+    if len(kset) == 1 and (q := aoc.first(kset)) not in forbidden:
+      count[q] += 1
+  return count
+
+def solve1(points, pool):
   bounds, count = aoc.bounds(points), Counter()
-  forbidden = find_forbidden(points, bounds)
-  for j in range(bounds.ymin, bounds.ymax + 1):
-    for i in range(bounds.xmin, bounds.xmax + 1):
-      kset = closest(j, i, points)
-      if len(kset) == 1 and (q := aoc.first(kset)) not in forbidden:
-        count[q] += 1
+  forbidden = find_forbidden(points, bounds, pool)
+  yrange = range(bounds.ymin, bounds.ymax + 1)
+  star_range = ((bounds, y, points, forbidden) for y in yrange)
+  for mask in pool.starmap(solve1_y, star_range):
+    count.update(mask)
   return max(count.values())
 
 def solve2(points, n):
@@ -41,5 +57,6 @@ def solve2(points, n):
   return area
 
 points = [aoc.ints(line.strip().split(", ")) for line in sys.stdin]
-aoc.cprint(solve1(points))
-aoc.cprint(solve2(points, 10000))
+with multiprocessing.Pool() as pool:
+  aoc.cprint(solve1(points, pool))
+  aoc.cprint(solve2(points, 10000))

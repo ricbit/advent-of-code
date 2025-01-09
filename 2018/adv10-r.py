45b404b0c09f642a3188553904b0a5c61a90251a
Ricardo Bittencourt <bluepenguin@gmail.com>
2025-01-08 22:48:14 -0300

    Problem 2018-10 under 1s

--- 2018/adv10-r.py
+++ 2018/adv10-r.py
@@ -1,29 +1,37 @@
 import aoc
+import numpy as np
+
+def draw(positions, ymin, ymax, xmin, xmax):
+  message = []
+  fastpoints = set(tuple(x) for x in positions)
+  for y in range(ymin, ymax + 1):
+    line = []
+    for x in range(xmin, xmax + 1):
+      line.append("#" if (y, x) in fastpoints else ".")
+    message.append("".join(line))
+  return message
 
 def solve(points):
-  positions = [[q.px, q.py] for q in points]
-  lastarea, ans = 1e15, (0, 0)
+  positions = np.array([[q.px, q.py] for q in points])
+  velocities = np.array([[q.vx, q.vy] for q in points])
+  lastarea, ans = 1e15, (0, [])
   for time in range(100000):
-    b = aoc.bounds(positions)
-    area = (b.ymax - b.ymin) * (b.xmax - b.xmin)
+    ymax = np.amax(positions[:, 0])
+    ymin = np.amin(positions[:, 0])
+    xmax = np.amax(positions[:, 1])
+    xmin = np.amin(positions[:, 1])
+    area = (ymax - ymin) * (xmax - xmin)
     if area > lastarea:
       for line in ans[1]:
         print(line)
       return ans[0]
     lastarea = area
     if area < 100 ** 2:
-      message = []
-      fastpoints = set(tuple(x) for x in positions)
-      for y in range(b.ymin, b.ymax + 1):
-        line = []
-        for x in range(b.xmin, b.xmax + 1):
-          line.append("#" if (y, x) in fastpoints else ".")
-        message.append("".join(line))
-      ans = (time, message)
-    for i in range(len(points)):
-      positions[i][0] += points[i].vx
-      positions[i][1] += points[i].vy
+      ans = (time, draw(positions, ymin, ymax, xmin, xmax))
+    positions += velocities
+  return None
 
-points = aoc.retuple_read("py_ px_ vy_ vx_",
+points = aoc.retuple_read(
+    "py_ px_ vy_ vx_",
     r".*?<(.*?), (.*)?>.*?<(.*?), (.*?)>")
 aoc.cprint(solve(points))

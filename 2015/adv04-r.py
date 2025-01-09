a0d929e8ba72a35d688355d38354b058511e2b32
Ricardo Bittencourt <bluepenguin@gmail.com>
2025-01-09 14:01:19 -0300

    Annotated 2015 4

--- 2015/adv04-r.py
+++ 2015/adv04-r.py
@@ -1,18 +1,35 @@
-import hashlib
 import aoc
 import itertools
 import sys
+import multiprocessing
 
-def search(seed, size):
-  zeros = "0" * size
+def eval_md5(i, zeros):
+  ans = aoc.md5(seed + str(i))
+  if ans.startswith(zeros):
+    return i
+  return None
+
+# multiprocessing is slower than single thread,
+# maybe there is a hidden mutex in the lib.
+def search_multi(zeros, pool):
+  fac = 128
+  for i in itertools.count(0, CPU * fac):
+    #ans = [eval_md5(ii, zeros) for ii in range(i, i + CPU)]
+    ans = list(pool.starmap(eval_md5, ((ii, zeros) for ii in range(i, i + CPU * fac)), fac))
+    valid = [m for m in ans if m is not None]
+    if valid:
+      return min(valid)
+  return None
+
+def search(zeros, pool):
   for i in itertools.count(0):
-    m = hashlib.md5()
-    m.update(bytes(seed, "ascii") + bytes(str(i), "ascii"))
-    ans = m.hexdigest()
-    if ans.startswith(zeros):
+    md5 = aoc.md5(seed + str(i))
+    if md5.startswith(zeros):
       return i
   return None
 
 seed = sys.stdin.read().strip()
-aoc.cprint(search(seed, 5))
-aoc.cprint(search(seed, 6))
+CPU = 1
+with multiprocessing.Pool(CPU) as pool:
+  aoc.cprint(search("0" * 5, pool))
+  aoc.cprint(search("0" * 6, pool))

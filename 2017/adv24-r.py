130a2478e96f41c02d83ccabc6e30394eb78c9bb
Ricardo Bittencourt <bluepenguin@gmail.com>
2025-01-10 20:59:29 -0300

    Optimized 2017-24

--- 2017/adv24-r.py
+++ 2017/adv24-r.py
@@ -1,24 +1,42 @@
 import sys
 import aoc
 import functools
+import multiprocessing
 
 @functools.lru_cache(maxsize=None)
-def build(ports, pos, used, size):
+def build(pos, used, size):
   ans = []
   for i, port in enumerate(ports):
     mask = 1 << i
     if mask & used != 0:
       continue
-    if port[0] == pos or port[1] == pos:
+    if pos in port:
       npos = port[0] + port[1] - pos
-      nsize, nstr = build(ports, npos, used + mask, size)
+      nsize, nstr = build(npos, used + mask, size)
       nsize = nsize + 1 if size is not None else None
       ans.append((nsize, port[0] + port[1] + nstr))
   return max(ans) if ans else (0, 0)
+
+def inner_build(pos, used, size, i):
+  mask = 1 << i
+  port = ports[i]
+  if mask & used != 0:
+    return None
+  if port[0] == pos or port[1] == pos:
+    npos = port[0] + port[1] - pos
+    nsize, nstr = build(npos, used + mask, size)
+    nsize = nsize + 1 if size is not None else None
+    return (nsize, port[0] + port[1] + nstr)
+  return None
+
+def base_build(pos, used, size, pool):
+  ans = pool.starmap(inner_build, ((pos, used, size, i) for i in range(len(ports))))
+  return max(s for s in ans if s is not None)
 
 lines = [line.strip() for line in sys.stdin]
 ports = []
 for line in lines:
   ports.append(tuple(int(i) for i in line.split("/")))
-aoc.cprint(build(tuple(ports), 0, 0, None)[1])
-aoc.cprint(build(tuple(ports), 0, 0, 0)[1])
+with multiprocessing.Pool() as pool:
+  aoc.cprint(base_build(0, 0, None, pool)[1])
+  aoc.cprint(base_build(0, 0, 0, pool)[1])

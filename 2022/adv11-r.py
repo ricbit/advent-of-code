a7dc8f29289d4abefd3b5b8582133075557f6bfa
Ricardo Bittencourt <bluepenguin@gmail.com>
2025-01-12 22:37:07 -0300

    Problem 2022-11 under 1s

--- 2022/adv11-r.py
+++ 2022/adv11-r.py
@@ -1,16 +1,18 @@
 import math
+import functools
 import heapq
 from dataclasses import dataclass
 import re
 import sys
 import aoc
 from collections import deque
+from typing import Callable
 
 @dataclass(repr=True, init=False)
 class Monkey:
   number: int
   items: list[int]
-  operation: str
+  operation: Callable[[int], int]
   divisible: int
   if_true: int
   if_false: int
@@ -35,7 +37,15 @@ def parse(lines):
     m = Monkey()
     m.number = int(search(0))
     m.items = deque(aoc.ints(search(1).split(",")))
-    m.operation = search(2)
+    operation = aoc.retuple("a op b", r"(\w+) (.) (\w+)", search(2))
+    if operation.op == "+":
+      if operation.a == "old" and operation.b.isdigit():
+        m.operation = functools.partial(lambda b, x: x + b, int(operation.b))
+    elif operation.op == "*":
+      if operation.a == "old" and operation.b.isdigit():
+        m.operation = functools.partial(lambda b, x: x * b, int(operation.b))
+      elif operation.a == "old" and operation.b == "old":
+        m.operation = lambda x: x * x
     m.divisible = int(search(3))
     m.if_true = int(search(4))
     m.if_false = int(search(5))
@@ -43,13 +53,15 @@ def parse(lines):
     monkeys.append(m)
   return monkeys
 
-def work_monkey(monkeys, current, worry, lcm):
+def work_monkey(monkeys, current, low_worry, lcm):
   while current.items:
     current.activity += 1
     old = current.items.popleft()
-    new = eval(current.operation)
-    item = new // worry
-    item %= lcm
+    new = current.operation(old)
+    if low_worry:
+      item = new // 3
+    else:
+      item = new % lcm
     if item % current.divisible == 0:
       monkeys[current.if_true].items.append(item)
     else:
@@ -59,13 +71,13 @@ def work_round(monkeys, worry, lcm):
   for i, current in enumerate(monkeys):
     work_monkey(monkeys, current, worry, lcm)
 
-def work_problem(rounds, worry):
+def work_problem(rounds, low_worry):
   monkeys = parse(lines)
   lcm = math.lcm(*[m.divisible for m in monkeys])
   for i in range(rounds):
-    work_round(monkeys, worry, lcm)
+    work_round(monkeys, low_worry, lcm)
   business = [m.activity for m in monkeys]
   return math.prod(heapq.nlargest(2, business))
 
-aoc.cprint(work_problem(20, 3))
-aoc.cprint(work_problem(10000, 1))
+aoc.cprint(work_problem(20, True))
+aoc.cprint(work_problem(10000, False))

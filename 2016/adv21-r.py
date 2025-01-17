fe4089c18510fe08c22ba2a7ea096ea7bb4a0205
Ricardo Bittencourt <bluepenguin@gmail.com>
2025-01-16 23:12:15 -0300

    Optimized problem 2016 21 under 1s

--- 2016/adv21-r.py
+++ 2016/adv21-r.py
@@ -1,20 +1,21 @@
 import sys
 import itertools
 import aoc
+import multiprocessing
 
-def action(lines, pwd):
+def action(pwd):
   for line in lines:
-    match line.strip().split():
+    match line:
       case "move", _, p0, _, _, p1:
         a = pwd.pop(int(p0))
         pwd.insert(int(p1), a)
       case "swap", "position", p0, _, _, p1:
         pwd[int(p0)], pwd[int(p1)] = pwd[int(p1)], pwd[int(p0)]
       case "swap", "letter", a, _, _, b:
-        for i in range(len(pwd)):
-          if pwd[i] == a:
+        for i, p in enumerate(pwd):
+          if p == a:
             pwd[i] = b
-          elif pwd[i] == b:
+          elif p == b:
             pwd[i] = a
       case "reverse", _, p0, _, p1:
         pwd[int(p0):int(p1) + 1] = pwd[int(p0):int(p1) + 1][::-1]
@@ -27,15 +28,22 @@ def action(lines, pwd):
       case "rotate", "based", _, _, _, _, x:
         i = pwd.index(x)
         size = i + 1 + (1 if i >= 4 else 0)
-        for j in range(int(size)):
+        for _ in range(int(size)):
           pwd = [pwd[-1]] + pwd[:-1]
   return "".join(pwd)
 
-def decode(lines, goal):
-  for p in itertools.permutations(goal, len(goal)):
-    if action(lines, list(p)) == goal:
-      return "".join(p)
+def search(src, goal):
+  return (src, action(list(src)) == goal)
 
-lines = [line.strip() for line in sys.stdin]
-aoc.cprint(action(lines, list("abcdefgh")))
-aoc.cprint(decode(lines, "fbgdceah")) 
+def decode(goal):
+  with multiprocessing.Pool() as pool:
+    perms = itertools.permutations(goal, len(goal))
+    ans = pool.starmap(search, ((p, goal) for p in perms))
+  for perm, found in ans:
+    if found:
+      return "".join(perm)
+  return None
+
+lines = [line.strip().split() for line in sys.stdin]
+aoc.cprint(action(list("abcdefgh")))
+aoc.cprint(decode("fbgdceah"))

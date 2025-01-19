8241a719ba2a630ea888fc5df61787021660c303
Ricardo Bittencourt <bluepenguin@gmail.com>
2025-01-19 09:41:17 -0300

    Improved problem 2020 15

--- 2020/adv15-r.py
+++ 2020/adv15-r.py
@@ -2,19 +2,19 @@ import sys
 import aoc
 
 def sequence(values, target):
-  vlink = [(v, i, None) for i, v in enumerate(values)]
+  vlink = [(i, None) for i, v in enumerate(values)]
   vhash = aoc.ddict(lambda: None)
   vhash.update({v: i for i, v in enumerate(values)})
   for _ in range(len(values), target):
-    _, i, prev = vlink[-1]
+    i, prev = vlink[-1]
     if prev is not None:
       new_value = i - prev
-      vlink.append((new_value, i + 1, vhash[new_value]))
+      vlink.append((i + 1, vhash[new_value]))
       vhash[new_value] = i + 1
     else:
-      vlink.append((0, i + 1, vhash[0]))
+      vlink.append((i + 1, vhash[0]))
       vhash[0] = i + 1
-  return vlink[-1][0]
+  return aoc.first(k for k, v in vhash.items() if v == vlink[-1][0])
 
 data = aoc.ints(sys.stdin.read().strip().split(","))
 aoc.cprint(sequence(data, 2020))

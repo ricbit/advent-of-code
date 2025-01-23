15fff3d3a150bd8b030175c9345c610130337929
Ricardo Bittencourt <bluepenguin@gmail.com>
2025-01-22 22:55:28 -0300

    Script sprint.sh accepts optional parameters

--- aoc/sprint.sh
+++ aoc/sprint.sh
@@ -1,7 +1,9 @@
-for i in `seq -f "%02g" 1 25` ; do 
+start=${1:-1}
+end=${2:-25}
+for i in `seq -f "%02g" $start $end` ; do 
   if [ -f adv$i-r.py ] ; then
     echo
     echo "Problem $i"
-    time python3.12 adv$i-r.py < input.$i.txt 
-  fi ;
+    time python3.13 adv$i-r.py < input.$i.txt 
+  fi 
 done

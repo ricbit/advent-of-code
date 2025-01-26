89a7b664ad513a6ac1f74f2acd1d346a0868597c
Ricardo Bittencourt <bluepenguin@gmail.com>
2025-01-26 00:45:18 -0300

    Raw problems 2021 23

--- 2021/raw/adv23-1.py
+++ 2021/raw/adv23-1.py
@@ -1,142 +1,140 @@
 import sys
+import string
 import re
 import itertools
 import math
+import cmath
 import aoc
 import heapq
 import functools
 import copy
-from collections import *
+from collections import Counter, deque
 from dataclasses import dataclass
+import networkx as nx
 
-def wrong_slot(pos, slots, i):
-  for name, frogs in zip("ABCD", pos):
-    for fy, fx, value in frogs:
-      if 2 <= fy <= 3 and fx == i and fx != slots[name]:
-        return True
-  return False
+# 0123456789012
+# #############
+# #...........#
+# ###A#D#B#D###
+#   #B#C#A#C#
+#   #########
 
-def occupied(y, x, j, i, pos):
-  for frogs in pos:
-    for fy, fx, value in frogs:
-      if (j, i) == (fy, fx):
-        return True
-  return False
+skip = [3, 5, 7, 9]
 
-def possible(m, pos, slots, name, y, x, value):
-  if value > 1:
-    return []
-  vnext = aoc.bq([(0, y, x)])
+def build_base_graph():
+  g = nx.Graph()
+  for i in range(1, 11):
+    if i not in skip:
+      j = i + 1
+      if j in skip:
+        j += 1
+      g.add_edge((1, i), (1, j), steps=j - i)
+  for i in skip:
+    g.add_edge((2, i), (1, i - 1), steps=2)
+    g.add_edge((2, i), (1, i + 1), steps=2)
+    g.add_edge((2, i), (3, i), steps=1)
+  nx.nx_agraph.write_dot(g, "p23.dot")
+  return g
+
+def get_frog_pos(state):
+  frog_pos = aoc.ddict(list)
+  for j, i in state.iter_all():
+    if state[j][i] in "ABCD":
+      frog_pos[state[j][i]].append((j, i))
+  return frog_pos
+
+def get_valid_moves(g, state, frog_pos):
+  proper_nest = lambda x: x[1] == skip[ord(frog) - ord("A")]
+  for frog, mpos in frog_pos.items():
+    for pos in mpos:
+      #print(f"frog {frog} at pos {pos} ")
+      newg = g.copy()
+      for obstacle in aoc.flatten(frog_pos.values()):
+        if obstacle != pos:
+          newg.remove_node(obstacle)
+      #print(list(newg.nodes))
+      #print(list(newg.edges))
+      for dst, size in nx.shortest_path_length(newg, source=pos, weight="steps").items():
+        # Can't walk on its own nest.
+        if dst[0] > 1 and pos[0] > 1 and dst[1] == pos[1]:
+          continue
+        # Can't walk on the corridor
+        if pos[0] == 1 and dst[0] == 1:
+          continue
+        # Can't enter in another frog's nest.
+        if dst[0] > 1 and not proper_nest(dst):
+          continue
+        # Can't walk from the right place
+        if pos[0] > 1 and dst[0] > 1 and proper_nest(pos) and proper_nest(dst):
+          continue
+        # Must enter the nest all the way
+        if dst[0] == 2 and proper_nest(dst) and state[3][dst[1]] == ".":
+          continue
+        # Must walk
+        if dst == pos:
+          continue
+        yield (frog, pos, dst, size) 
+  return []
+
+def encode_state(state):
+  return "".join("".join(line) for line in state.table)
+
+def decode_state(encoded):
+  return aoc.Table([list(encoded[13 * i: 13 * i + 13]) for i in range(5)])
+
+def print_state(state):
+  for line in state.table:
+    print("".join(line))
+  print()
+
+def heuristic(state):
+  h = 0
+  for frog, pos in get_frog_pos(state).items():
+    for j, i in pos: 
+      if j <= 1 or (i != skip[ord(state[j][i]) - ord("A")]):
+        factor = abs(i - skip[ord(state[j][i]) - ord("A")])
+        if (i != skip[ord(state[j][i]) - ord("A")]):
+          factor += 1 + (2 - j)
+        factor *= 10 ** (ord(frog) - ord('A'))
+        h += factor
+  return h
+
+def solve(start):
+  pnext = [(heuristic(start), 0, encode_state(start))]
+  g = build_base_graph()
   visited = set()
-  allowed = []
-  #print(f"from {name} {y} {x} {value}")
-  while vnext:
-    score, y, x = vnext.pop()
-    #print(f"try {y} {x}")
-    for j, i in m.iter_neigh4(y, x, lambda c: c != "#"):
-      #print(f"check {j} {i}")
-      if (j, i) not in visited:
-        #print(f"enter {j} {i}")
-        if occupied(y, x, j, i, pos):
-          continue
-        #print(f"noc {j} {i}")
-        vnext.push((score + 1, j, i))
-        visited.add((j, i))
-        if j == 1 and i in slots.values():
-          continue
-        if j == 1 and value == 1:
-          continue
-        if i in slots.values() and i != slots[name]:
-          continue
-        if i == slots[name]:
-          if wrong_slot(pos, slots, i): 
-            continue
-        allowed.append((score + 1, j, i, value + 1 if value == 0 else 1))
-  #print(allowed)
-  return allowed
-
-def finished(npos, slots):
-  for name, frogs in zip("ABCD", npos):
-    c = 0
-    for fy, fx, value in frogs:
-      if fx == slots[name] and fy in [2, 3]:
-        c += 1
-    if c != 2:
-      return False
-  return True
-
-def print_table(m, pos, slots):
-  lines = []
-  for j in range(len(m.table)):
-    line = []
-    for i in range(len(m.table[0])):
-      line.append("." if m[j][i] in "ABCD" else m[j][i])
-    lines.append(line)
-  for nf, (name, frogs) in enumerate(zip("ABCD", pos)):
-    for nff, (fy, fx, value) in enumerate(frogs): 
-      lines[fy][fx] = name
-  return "\n".join("".join(line) for line in lines)
-
-def canonize(npos):
-  ans = []
-  for frog in npos:
-    ans.append(tuple(sorted(frog)))
-  return tuple(ans)
-
-def heuristic(pos, slots, cost):
-  ans = 0
-  return ans
-  for name, frogs in zip("ABCD", pos):
-    for fy, fx, value in frogs:
-      ans += abs(fx - slots[name]) * cost[name]
-  return ans
-
-
-def search(m, pos, slots):
-  pos = tuple(tuple(v) for k, v in sorted(pos.items()))
-  cost = dict(zip("ABCD", [1, 10, 100, 1000]))
-  vnext = [(heuristic(pos, slots, cost), 0, pos, "")]
-  visited = set()
-  c = 0
-  while vnext:
-    h, score, pos, path = heapq.heappop(vnext)
-    if c % 100 == 0:
-      print(score, pos, len(vnext))
-    c += 1
-    if finished(pos, slots):
-      print(path)
+  estimate = {}
+  count = 0
+  while pnext:
+    heur, score, state = heapq.heappop(pnext)
+    if state in visited:
+      continue
+    visited.add(state)
+    state = decode_state(state)
+    if heuristic(state) == 0:
       return score
-    #print_table(m, pos, slots)
-    for nf, (name, frogs) in enumerate(zip("ABCD", pos)):
-      for nff, (fy, fx, value) in enumerate(frogs): 
-        for nscore, j, i, nvalue in possible(m, pos, slots, name, fy, fx, value):
-          npos = [list(b) for b in pos]
-          npos[nf][nff] = (j, i, nvalue)
-          npos = canonize(npos)
-          if npos not in visited:
-            s = score + nscore * cost[name] 
-            heapq.heappush(vnext, (s, s, npos, path + "\n\n" + print_table(m, npos, slots)))
-            #heapq.heappush(vnext, ((heuristic(pos, slots, cost) + s, s, npos)))
-            visited.add(npos)
+    frog_pos = get_frog_pos(state)
+    for frog, src, dst, steps in get_valid_moves(g, state, frog_pos):
+      nstate = state.copy()
+      nstate[src[0]][src[1]] = "."
+      nstate[dst[0]][dst[1]] = frog
+      factor = 10 ** (ord(frog) - ord('A'))
+      tstate = encode_state(nstate)
+      if tstate not in visited:
+        newscore = score + steps * factor
+        h = newscore + heuristic(nstate)
+        if h < estimate.get(tstate, 10e6):
+          count += 1
+          if count % 1000 == 0:
+            print(f"from score {score} heur {heur}, src {src}, dst {dst} ")
+            #print_state(state)
+            #print(f"to score {score} heur {heur}")
+            #print_state(nstate)
+            #print("--- \n")
+          heapq.heappush(pnext, (h, newscore, tstate))
+          estimate[tstate] = h
+  return data
 
-def get_pos(m):
-  pos = aoc.ddict(lambda: [])
-  for j, i in m.iter_all():
-    print(j, i)
-    if m[j][i].isupper():
-      pos[m[j][i]].append((j, i, 0))
-  slots = []
-  for i in range(len(m[3])):
-    if m[3][i].isupper():
-      slots.append(i)
-      m[2][i] = m[3][i] = "."
-  return pos, dict(zip("ABCD", slots))
-
-lines = [line.rstrip() for line in sys.stdin]
-w = max(len(line) for line in lines)
-lines = [list((line + " " * w)[:w]) for line in lines]
-for line in lines:
-  print(line)
-m = aoc.Table(lines)
-pos, slots = get_pos(m)
-print(search(m, pos, slots))
+data = [list((line + " " * 8)[:13]) for line in sys.stdin.read().splitlines()]
+data = aoc.Table(data)
+aoc.cprint(solve(data))
--- 2021/raw/adv23-2.py
+++ 2021/raw/adv23-2.py
@@ -1,142 +1,149 @@
 import sys
-import copy
+import string
+import re
+import itertools
+import math
+import cmath
+import aoc
 import heapq
+import functools
+import copy
+from collections import Counter, deque
+from dataclasses import dataclass
+import networkx as nx
 
-base = [list((line.rstrip() + " " * 13)[:13]) for line in sys.stdin]
-w, h = len(base[0]), len(base)
+# 0123456789012
+# #############
+# #...........#
+# ###A#D#B#D###
+#   #B#C#A#C#
+#   #########
 
-START = 0
-MOVED = 1
-STOPPED = 2
+skip = [3, 5, 7, 9]
 
-def neigh(j, i):
-  yield j - 1, i
-  yield j + 1, i
-  yield j, i - 1
-  yield j, i + 1
+def build_base_graph():
+  g = nx.Graph()
+  for i in range(1, 11):
+    if i not in skip:
+      j = i + 1
+      if j in skip:
+        j += 1
+      g.add_edge((1, i), (1, j), steps=j - i)
+  for i in skip:
+    g.add_edge((2, i), (1, i - 1), steps=2)
+    g.add_edge((2, i), (1, i + 1), steps=2)
+    g.add_edge((2, i), (3, i), steps=1)
+    g.add_edge((3, i), (4, i), steps=1)
+    g.add_edge((4, i), (5, i), steps=1)
+  nx.nx_agraph.write_dot(g, "p23.dot")
+  return g
 
-def get_empty(base):
-  empty = copy.deepcopy(base)
-  for j in range(h):
-    for i in range(w):
-      if empty[j][i] >= "A" and empty[j][i] <= "D":
-        empty[j][i] = "."
-  return empty
+def get_frog_pos(state):
+  frog_pos = aoc.ddict(list)
+  for j, i in state.iter_all():
+    if state[j][i] in "ABCD":
+      frog_pos[state[j][i]].append((j, i))
+  return frog_pos
 
-def parse_start(base):
-  first = set()
-  for j in range(h):
-    for i in range(w):
-      if base[j][i] >= "A" and base[j][i] <= "D":
-        yield (j, i, base[j][i], START)
-        first.add(base[j][i])
+def empty_below(frog, dst, state):
+  nest = skip[ord(frog) - ord("A")]
+  if dst[1] != nest:
+    return False
+  for i in range(dst[0] + 1, 6):
+    if state[i][nest] != frog:
+      return True
+  return False
 
-def price(letter, value):
-  return value * (10 ** (ord(letter) - ord("A")))
+def get_valid_moves(g, state, frog_pos):
+  proper_nest = lambda x: x[1] == skip[ord(frog) - ord("A")]
+  for frog, mpos in frog_pos.items():
+    for pos in mpos:
+      newg = g.copy()
+      for obstacle in aoc.flatten(frog_pos.values()):
+        if obstacle != pos:
+          newg.remove_node(obstacle)
+      for dst, size in nx.shortest_path_length(newg, source=pos, weight="steps").items():
+        # Can't walk inside a nest.
+        if dst[0] > 1 and pos[0] > 1 and dst[1] == pos[1]:
+          continue
+        # Can't walk on the corridor
+        if pos[0] == 1 and dst[0] == 1:
+          continue
+        # Can't enter in another frog's nest.
+        if dst[0] > 1 and not proper_nest(dst):
+          continue
+        # Must enter the nest all the way
+        if empty_below(frog, dst, state):
+          continue
+        # Must walk
+        if dst == pos:
+          continue
+        yield (frog, pos, dst, size) 
+  return
 
-def candidates(base, amphi):
-  j, i, letter, moved = amphi
-  state = [(j, i, 0)]
-  visited = [[False] * w for i in range(h)]
-  while state:
-    j, i, cost = state.pop(0)
-    for jj, ii in neigh(j, i):
-      if base[jj][ii] == "." and not visited[jj][ii]:
-        visited[jj][ii] = True
-        yield (jj, ii, price(letter, cost + 1))
-        state.append((jj, ii, cost + 1))
+def encode_state(state):
+  return "".join("".join(line) for line in state.table)
 
-def valid(base, amphi, candidates):
-  j, i, letter, moved = amphi
-  for jj, ii, cost in candidates:
-    if moved == STOPPED:
+def decode_state(encoded):
+  return aoc.Table([list(encoded[13 * i: 13 * i + 13]) for i in range(7)])
+
+def print_state(state):
+  for line in state.table:
+    print("".join(line))
+  print()
+
+def heuristic(state):
+  h = 0
+  for frog, pos in get_frog_pos(state).items():
+    for src in pos:
+      nest = skip[ord(frog) - ord("A")]
+      factor = 10 ** (ord(frog) - ord('A'))
+      h += abs(src[1] - nest) * factor
+      if src[1] != nest:
+        h += (src[0] - 1) * factor
+  return h
+
+def solve(start):
+  pnext = [(heuristic(start), 0, encode_state(start))]
+  g = build_base_graph()
+  visited = set()
+  estimate = {}
+  count = 0
+  while pnext:
+    heur, score, state = heapq.heappop(pnext)
+    if state in visited:
       continue
-    if j > 1 and jj > 1:
-      continue
-    if jj == 1 and ii in [3, 5, 7, 9]:
-      continue
-    if jj == 1 and moved != START:
-      continue
-    if jj != 1 and moved != MOVED:
-      continue
-    if jj != 1:
-      if ii != 3 + 2 * (ord(letter) - ord('A')):
-        continue
-      if jj == 2 and (base[3][ii] == "." or base[3][ii] != letter):
-        continue
-    yield jj, ii, cost
+    visited.add(state)
+    state = decode_state(state)
+    if heuristic(state) == 0:
+      return score
+    frog_pos = get_frog_pos(state)
+    for frog, src, dst, steps in get_valid_moves(g, state, frog_pos):
+      nstate = state.copy()
+      nstate[src[0]][src[1]] = "."
+      nstate[dst[0]][dst[1]] = frog
+      factor = 10 ** (ord(frog) - ord('A'))
+      tstate = encode_state(nstate)
+      if tstate not in visited:
+        newscore = score + steps * factor
+        h = newscore + heuristic(nstate)
+        if h < estimate.get(tstate, 10e6):
+          count += 1
+          if count % 1000 == 0:
+            print(f"from score {score} heur {heur}, src {src}, dst {dst} "
+                  f"iter {count} size {len(pnext)}")
+            print_state(state)
+            print(f"to score {score} heur {heur}")
+            print_state(nstate)
+            print("--- \n")
+          heapq.heappush(pnext, (h, newscore, tstate))
+          estimate[tstate] = h
+  return data
 
-def fill_base(state):
-  empty = get_empty(base)
-  for j, i, letter, moved in state:
-    empty[j][i] = letter
-  return empty
-
-def smallest_path(amphis, y, x, goal):
-  ans = []
-  for j, i, letter, moved in amphis:
-    if goal == letter:
-      if i == x and j == y:
-        return 0
-      else:
-        if j == 1:
-          cost = j - 1 + abs(i - x)
-        else:
-          cost = j - 1 + j - 1 + abs(i - x)
-        ans.append(price(letter, cost))
-        #ans.append(cost)
-  return min(ans)
-
-def distance_base(amphis):
-  ans = 0
-  for j, i, letter, moved in amphis:
-    ii = 3 + 2 * (ord(letter) - ord("A"))
-    ans += price(letter, abs(ii - i))
-  return ans
-
-def update(amphis, j, i, jj, ii, letter):
-  for y, x, letter, moved in amphis:
-    if y == j and x == i:
-      moved += 1
-      if moved > 2: 
-        moved = 2
-      yield (jj, ii, letter, moved)
-    else:
-      yield (y, x, letter, moved)
-
-def print_state(amphis, estimate, cost):
-  current_base = fill_base(amphis)
-  print(amphis)
-  print("\n".join("".join(line) for line in current_base))
-  print("\n%d %d\n" % (estimate, cost))
-
-def search(base):
-  start_amphi = tuple(parse_start(base))
-  states = [(0, 0, start_amphi)]
-  visited = {start_amphi: 0}
-  while states:
-    estimate, cost, amphis = heapq.heappop(states)
-    #cost, estimate, amphis = heapq.heappop(states)
-    if amphis in visited and estimate > visited[amphis]:
-      continue
-    print("%d %d %d" % (estimate, cost, len(states)))
-    print_state(amphis, estimate, cost)
-    current_base = fill_base(amphis)
-    for amphi in amphis:
-      j, i, letter, moved = amphi
-      for candidate in valid(current_base, amphi, candidates(current_base, amphi)):
-        jj, ii, price = candidate
-        new_cost = cost + price
-        new_amphi = tuple(sorted(update(amphis, j, i, jj, ii, letter)))
-        new_distance =  distance_base(new_amphi)
-        new_estimate = new_cost + new_distance
-        if new_distance == 0:
-          print_state(new_amphi, new_estimate, new_cost)
-          return
-        new_state = (new_estimate, new_cost, new_amphi)
-        #new_state = (new_cost, new_estimate, new_amphi)
-        if new_amphi not in visited or visited[new_amphi] > new_estimate:
-          visited[new_amphi] = new_estimate
-          heapq.heappush(states, new_state)
-
-search(base)
+data = [list((line + " " * 8)[:13]) for line in sys.stdin.read().splitlines()]
+data.insert(-2, list("  #D#C#B#A#  "))
+data.insert(-2, list("  #D#B#A#C#  "))
+for line in data:
+  print("".join(x for x in line) + "5")
+data = aoc.Table(data)
+aoc.cprint(solve(data))

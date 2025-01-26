d184ebb324ca0f2007a79b9c529aa16771e64b21
Ricardo Bittencourt <bluepenguin@gmail.com>
2025-01-26 19:49:34 -0300

    Optimized problem 2021 23

--- 2021/adv23-r.py
+++ 2021/adv23-r.py
@@ -13,7 +13,7 @@ import functools
 
 skip = [3, 5, 7, 9]
 
-def build_base_graph():
+def build_base_graph(part1):
   g = nx.Graph()
   for i in range(1, 11):
     if i not in skip:
@@ -25,8 +25,9 @@ def build_base_graph():
     g.add_edge((2, i), (1, i - 1), steps=2)
     g.add_edge((2, i), (1, i + 1), steps=2)
     g.add_edge((2, i), (3, i), steps=1)
-    g.add_edge((3, i), (4, i), steps=1)
-    g.add_edge((4, i), (5, i), steps=1)
+    if not part1:
+      g.add_edge((3, i), (4, i), steps=1)
+      g.add_edge((4, i), (5, i), steps=1)
   nx.nx_agraph.write_dot(g, "p23.dot")
   return g
 
@@ -39,9 +40,10 @@ def get_frog_pos(state):
 
 def empty_below(frog, dst, state):
   nest = skip[ord(frog) - ord("A")]
+  limit = state.h - 1
   if dst[1] != nest:
     return False
-  for i in range(dst[0] + 1, 6):
+  for i in range(dst[0] + 1, limit):
     if state[i][nest] != frog:
       return True
   return False
@@ -76,8 +78,9 @@ def get_valid_moves(g, state, frog_pos):
 def encode_state(state):
   return "".join("".join(line) for line in state.table)
 
-def decode_state(encoded):
-  return aoc.Table([list(encoded[13 * i: 13 * i + 13]) for i in range(7)])
+def decode_state(encoded, part1):
+  limit = 5 if part1 else 7
+  return aoc.Table([list(encoded[13 * i: 13 * i + 13]) for i in range(limit)])
 
 def print_state(state):
   for line in state.table:
@@ -93,20 +96,24 @@ def heuristic(state):
       h += abs(src[1] - nest) * factor
       if src[1] != nest:
         h += (src[0] - 1) * factor
+  for frog, s in zip("ABCD", skip):
+    factor = 10 ** (ord(frog) - ord('A'))
+    for j in range(2, state.h - 1):
+      if state[j][s] != frog:
+        h += (j - 1) * factor
   return h
 
-def solve(start):
+def solve(start, part1=True):
   pnext = [(heuristic(start), 0, encode_state(start))]
-  g = build_base_graph()
+  g = build_base_graph(part1)
   visited = set()
-  estimate = {}
   count = 0
   while pnext:
     heur, score, state = heapq.heappop(pnext)
     if state in visited:
       continue
     visited.add(state)
-    state = decode_state(state)
+    state = decode_state(state, part1)
     if heuristic(state) == 0:
       return score
     frog_pos = get_frog_pos(state)
@@ -119,22 +126,22 @@ def solve(start):
       if tstate not in visited:
         newscore = score + steps * factor
         h = newscore + heuristic(nstate)
-        if h < estimate.get(tstate, 10e6):
-          count += 1
-          if count % 1000 == 0:
-            pass
-            print(f"from score {score} heur {heur}, src {src}, dst {dst} "
-                  f"iter {count} size {len(pnext)}")
-            print_state(state)
-            print(f"to score {score} heur {heur}")
-            print_state(nstate)
-            print("--- \n")
-          heapq.heappush(pnext, (h, newscore, tstate))
-          estimate[tstate] = h
+        count += 1
+        if count % 1000 == 0:
+          pass
+          #print(f"from score {score} heur {heur}, src {src}, dst {dst} "
+          #      f"iter {count} size {len(pnext)}")
+          #print_state(state)
+          #print(f"to score {score} heur {heur}")
+          #print_state(nstate)
+          #print("--- \n")
+        heapq.heappush(pnext, (h, newscore, tstate))
   return data
 
 data = [list((line + " " * 8)[:13]) for line in sys.stdin.read().splitlines()]
+table = aoc.Table(data)
+aoc.cprint(solve(table, part1=True))
 data.insert(-2, list("  #D#C#B#A#  "))
 data.insert(-2, list("  #D#B#A#C#  "))
-data = aoc.Table(data)
-aoc.cprint(solve(data))
+table = aoc.Table(data)
+aoc.cprint(solve(table, part1=False))

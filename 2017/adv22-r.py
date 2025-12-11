af26683617e6cb3b51ef73a6dcc7e67326889bb2
Ricardo Bittencourt <bluepenguin@gmail.com>
2025-12-10 22:33:37 -0300

    Optimized 2017-22

--- 2017/adv22-r.py
+++ 2017/adv22-r.py
@@ -1,46 +1,40 @@
 import aoc
 import copy
 
-def rule1(infected, vdir, y, x, ans):
-  if infected[(y, x)] == "#":
+def rule1(infected, vdir, pos, ans):
+  if infected[pos] == "#":
     vdir *= 1j
-    infected[(y, x)] = "."
+    infected[pos] = "."
   else:
     vdir *= -1j
-    infected[(y, x)] = "#"
+    infected[pos] = "#"
     ans += 1
   return vdir, ans
 
-def rule2(infected, vdir, y, x, ans):
-  match infected[(y, x)]:
-    case ".":
-      vdir *= -1j
-      infected[(y, x)] = "W"
-    case "W":
-      infected[(y, x)] = "#"
-      ans += 1
-    case "#":
-      vdir *= 1j
-      infected[(y, x)] = "F"
-    case "F":
-      vdir *= -1
-      infected[(y, x)] = "."
+cdir = dict(zip(".W#F", [-1j, 1, 1j, -1]))
+cinfected = dict(zip(".W#F", "W#F."))
+
+def rule2(infected, vdir, pos, ans):
+  current = infected[pos]
+  vdir *= cdir[current]
+  infected[pos] = cinfected[current]
+  if current == "W":
+    ans += 1
   return vdir, ans
 
 def govirus(infected, y, x, vdir, n, rule):
   ans = 0
+  pos = (y * 1j + x)
   for _ in range(n):
-    vdir, ans = rule(infected, vdir, y, x, ans)
-    p = (y * 1j + x) + vdir
-    y = p.imag
-    x = p.real
+    vdir, ans = rule(infected, vdir, pos, ans)
+    pos += vdir
   return ans
 
 t = aoc.Table.read()
 infected = aoc.ddict(lambda: ".")
 for j, i in t.iter_all():
   if t[j][i] == "#":
-    infected[(j, i)] = "#"
+    infected[j * 1j + i] = "#"
 m = len(t.table) // 2
 infected2 = copy.deepcopy(infected)
 aoc.cprint(govirus(infected, m, m, -1j, 10000, rule1))
